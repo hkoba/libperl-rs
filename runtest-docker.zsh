6f9f507e4346e4812a084bca2dfd0a691c0344fc
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-09-28 14:09:41 +0900

    Add travis-ci

--- runtest-docker.zsh
+++ runtest-docker.zsh
@@ -14,7 +14,8 @@ fi
 
 #========================================
 
-zparseopts -D -K x=o_xtrace -image:=o_image
+zparseopts -D -K x=o_xtrace \
+           -image:=o_image -target:=o_targetDir
 
 if (($#o_xtrace)); then set -x; fi
 
@@ -24,17 +25,27 @@ else
     imageName=perl
 fi
 
+volOpts=()
+if (($#o_targetDir)); then
+    volOpts+=(-v ${o_targetDir[2]#=}:/app/target${SELINUX})
+fi
+
 if ((! ARGC)); then
     argv=(--all)
 fi
 
 #========================================
 
-exec docker run --rm -it -v $topDir:/app${SELINUX} $imageName /bin/bash -c '
+exec docker run --rm -it \
+     -v $topDir:/app${SELINUX} \
+     $volOpts \
+     $imageName /bin/bash -c '
  apt update &&
  apt install -y llvm-dev libclang-dev clang &&
  curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh &&
  sh rustup.sh -y &&
  source $HOME/.cargo/env &&
  cd /app &&
+ cargo clean &&
+ cargo build -vv &&
  cargo test '"$*"
