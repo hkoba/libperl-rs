c9e566ca46f241a90e2899c6c0ddfc739d11b8c1
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-09-29 13:24:55 +0900

    Add examples/100_scan_ops.rs

---
+++ examples/100_scan_ops.rs
@@ -0,0 +1,40 @@
+use libperl_rs::perl::Perl;
+use libperl_sys::{op, PL_op_name};
+
+use std::env;
+use std::ffi::CStr;
+
+fn scan_ops(mut op: *const op) {
+    while !op.is_null() {
+        let ty = unsafe {(*op).op_type()};
+        // let op_name = unsafe {
+        //     slice::from_raw_parts(
+        //         c_perl::PL_op_name as *const c_char,
+        //         c_perl::PL_maxo as usize
+        //     );
+        // };
+        print!("op={:#?} {}\n",
+               op, 
+               // op_name[ty]
+               unsafe {
+                   CStr::from_ptr(PL_op_name[ty as usize])
+               }
+               .to_str().unwrap()
+        );
+        op = unsafe {(*op).op_next as *const op};
+    }
+}
+
+
+fn test() {
+    let mut perl = Perl::new();
+    perl.parse_env_args(env::args(), env::vars());
+    scan_ops(unsafe {*perl.my_perl}.Imain_start);
+}
+
+
+// cargo run --example 100_list_ops -- -le 'print "FOO"'
+
+fn main() {
+    test();
+}
