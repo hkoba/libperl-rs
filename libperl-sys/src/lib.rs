5ddd996a77a2efdd246042317c58e94f088c4be5
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-09-29 15:01:23 +0900

    Move include of generated code to perl_core.rs

--- libperl-sys/src/lib.rs
+++ libperl-sys/src/lib.rs
@@ -1,14 +1,12 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+pub mod perl_core;
+pub use perl_core::*;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let perl = perl_alloc();
     }
 }
