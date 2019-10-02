9ff658fe8d7a2a4b87f1fbeedf5b0f91b6770fb9
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-10-02 15:20:50 +0900

    Conditional compilation worked! but found PL_op_name is empty for not(useithreads). Why?

--- examples/100_scan_ops.rs
+++ examples/100_scan_ops.rs
@@ -25,15 +25,22 @@ fn scan_ops(mut op: *const op) {
     }
 }
 
-
+#[cfg(perl_useithreads)]
 fn test() {
     let mut perl = Perl::new();
     perl.parse_env_args(env::args(), env::vars());
-    if cfg!(perl_useithreads) {
+    // if cfg!(perl_useithreads) {
         scan_ops(unsafe {*perl.my_perl}.Imain_start);
-    } else {
-        println!("Nothing nown about Imain_start");
-    }
+    // }
+}
+
+#[cfg(not(perl_useithreads))]
+fn test() {
+    let mut perl = Perl::new();
+    perl.parse_env_args(env::args(), env::vars());
+    // if cfg!(perl_useithreads) {
+         scan_ops(unsafe {libperl_sys::PL_main_start});
+    // }
 }
 
 
