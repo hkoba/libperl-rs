a66dc93b6ebebc4c7111e35a5e59c3f14d3e1c3f
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-10-06 10:12:34 +0900

    Install rustfmt

--- runtest-docker.zsh
+++ runtest-docker.zsh
@@ -48,6 +48,7 @@ cmdList=(
     'curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh'
     'sh rustup.sh -y'
     'source $HOME/.cargo/env'
+    'rustup component add rustfmt'
     'cd /app'
 )
 
