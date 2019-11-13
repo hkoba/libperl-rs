f589a3ccb1f55116c86efc365c4acdeebafa9767
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-11-13 21:47:53 +0900

    Move op_walker1::Walker into 104_enum_op_tree and rewrite it with sibling_iter()

--- examples/104_enum_op_tree.rs
+++ examples/104_enum_op_tree.rs
@@ -1,15 +1,33 @@
 use std::env;
 
+use libperl_sys::op;
 use libperl_rs::Perl;
 
 mod eg;
+use eg::op0::*;
+
+#[cfg(perlapi_ver26)]
+pub struct Walker<'a> {
+    pub perl: &'a Perl,
+}
+
+#[cfg(perlapi_ver26)]
+impl<'a> Walker<'a> {
+    pub fn walk(&'a self, o: *const op, level: isize) {
+        for kid in sibling_iter(o) {
+            self.walk(kid, level+1);
+        }
+        print!("{}", "  ".repeat(level as usize));
+        println!("{:?} {:?}", op_name(o), op_extract(&self.perl, o));
+    }
+}
 
 #[cfg(perlapi_ver26)]
 fn my_test() {
     let mut perl = Perl::new();
     perl.parse_env_args(env::args(), env::vars());
     
-    let walker = eg::op_walker1::Walker {perl: &perl};
+    let walker = Walker {perl: &perl};
 
     walker.walk(perl.get_main_root(), 0);
 }
--- examples/eg/mod.rs
+++ examples/eg/mod.rs
@@ -1,6 +1,4 @@
 #[allow(dead_code)]
-pub mod op_walker1;
-#[allow(dead_code)]
 pub mod pad0;
 #[allow(dead_code)]
 pub mod op0;
