6de4412dee66754c973713d7ee3ff7ade49090db
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-11-13 14:43:30 +0900

    Rewrite 105_scan_stash with HvIter (iterator for HV)

--- examples/105_scan_stash.rs
+++ examples/105_scan_stash.rs
@@ -1,7 +1,8 @@
 use std::env;
-use std::ffi::CStr;
 
 use libperl_rs::*;
+
+mod eg;
 
 fn my_test() {
     let mut perl = Perl::new();
@@ -9,24 +10,14 @@ fn my_test() {
     
     // get_cvstash(perl.get_main_cv()); returns null. Why?
     
-    assert_eq!(perl.gv_stashpv("main", 0), perl.get_defstash());
-    
     let stash = perl.get_defstash();
     
-    unsafe {libperl_sys::Perl_hv_iterinit(perl.my_perl, stash)};
+    assert_eq!(perl.gv_stashpv("main", 0), stash);
     
-    let mut he = unsafe {libperl_sys::Perl_hv_iternext(perl.my_perl, stash)};
-    while !he.is_null() {
-        let mut nlen: i32 = 0;
-        let name = unsafe {CStr::from_ptr(unsafe {libperl_sys::Perl_hv_iterkey(perl.my_perl, he, &mut nlen)})};
-        let val = unsafe {libperl_sys::Perl_hv_iterval(perl.my_perl, stash, he)};
-        println!("name = {:?} value = {:?}", name, val);
-        he = unsafe {libperl_sys::Perl_hv_iternext(perl.my_perl, stash)};
+    for (name, value) in eg::hv_iter0::HvIter::new(&perl, stash) {
+        println!("name = {:?} value = {:?}", name, value);        
     }
 }
-
-#[cfg(not(perlapi_ver26))]
-fn my_test() {}
 
 fn main() {
     my_test();
