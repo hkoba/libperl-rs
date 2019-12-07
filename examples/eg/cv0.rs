de694c4ed2a60a54974534a3bf4141fdad6f7c17
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-12-07 16:41:29 +0900

    CvROOT, CvSTART should return null for XSUB

--- examples/eg/cv0.rs
+++ examples/eg/cv0.rs
@@ -1,21 +1,31 @@
+#![allow(non_snake_case)]
+
 use libperl_sys;
 use super::sv0::*;
 
-#[allow(non_snake_case)]
 pub fn CvSTART(cv: *const libperl_sys::cv) -> *const libperl_sys::OP {
     assert_eq!(SvTYPE(cv as *const libperl_sys::sv), svtype::SVt_PVCV);
     let xpvcv = unsafe {(*cv).sv_any as *const libperl_sys::xpvcv};
+    if (unsafe {*xpvcv}.xcv_flags & libperl_sys::CVf_ISXSUB) != 0 {
+        return std::ptr::null()
+    }
     unsafe {(*xpvcv).xcv_start_u.xcv_start}
 }
 
-#[allow(non_snake_case)]
 pub fn CvROOT(cv: *const libperl_sys::cv) -> *const libperl_sys::OP {
     assert_eq!(SvTYPE(cv as *const libperl_sys::sv), svtype::SVt_PVCV);
     let xpvcv = unsafe {(*cv).sv_any as *const libperl_sys::xpvcv};
+    if (unsafe {*xpvcv}.xcv_flags & libperl_sys::CVf_ISXSUB) != 0 {
+        return std::ptr::null()
+    }
     unsafe {(*xpvcv).xcv_root_u.xcv_root}
 }
 
-#[allow(non_snake_case)]
+pub fn CvFLAGS(cv: *const libperl_sys::cv) -> u32 {
+    let xpvcv = unsafe {(*cv).sv_any as *const libperl_sys::xpvcv};
+    unsafe {*xpvcv}.xcv_flags
+}
+
 pub fn CvFILE(cv: *const libperl_sys::cv) -> Option<String> {
     if cv.is_null() {
         return None
