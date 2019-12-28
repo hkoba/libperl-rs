970b96f0d69d578fd680a679ff2affb13779e828
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-12-28 23:23:34 +0900

    wip - OpExtractor

--- examples/108_scan_subs_in_a_file2.rs
+++ examples/108_scan_subs_in_a_file2.rs
@@ -7,41 +7,23 @@ use libperl_rs::*;
 #[cfg(perlapi_ver26)]
 mod eg;
 #[cfg(perlapi_ver26)]
-use eg::{op0::*,sv0::*,cv0::*,stash_walker0::*};
-
-#[cfg(perlapi_ver26)]
-pub struct OpWalker<'a> {
-    pub perl: &'a Perl,
-    pub cv: *const libperl_sys::cv,
-}
-
-#[cfg(perlapi_ver26)]
-impl<'a> OpWalker<'a> {
-    pub fn walk(&'a self, o: *const op, level: isize) {
-        if o.is_null() {return}
-        print!("{}", "  ".repeat(level as usize));
-        let ox = op_extract(&self.perl, self.cv, o);
-        println!("{:?} {:?}", op_name(o), ox);
-        for kid in sibling_iter(o) {
-            self.walk(kid, level+1);
-        }
-    }
-}
+use eg::{op1::*,sv0::*,cv0::*,stash_walker0::*};
 
 #[cfg(perlapi_ver26)]
 fn my_test() {
     let mut perl = Perl::new();
     perl.parse_env_args(env::args(), env::vars());
     
+    let mut op_extractor = OpExtractor::new(&perl);
+
     let main_file = sv_extract_pv(perl.get_sv("0", 0)).unwrap();
     println!("$0 = {:?}", main_file);
     
     let filter = |cv| CvFILE(cv).map_or(false, |s| s == main_file);
 
     let mut emitter = |name: &String, cv: *const libperl_sys::cv| {
-        let walker = OpWalker {perl: &perl, cv};
         println!("sub {:?}", name);
-        walker.walk(CvROOT(cv), 0);
+        println!("{:?}", op_extractor.extract(cv, CvROOT(cv)));
         println!("");
     };
 
--- examples/eg/op1.rs
+++ examples/eg/op1.rs
@@ -9,95 +9,116 @@ use super::pad0::*;
 use super::op0::{op_sibling, op_sv_or, Name};
 pub use super::op0::op_name;
 
+use typed_arena::Arena;
+
+pub struct OpExtractor<'a> {
+    perl: &'a Perl,
+    ops: Arena<Op<'a>>,
+}
+
 #[allow(non_camel_case_types)]
 #[derive(Debug)]
-pub enum Op/* <'a>*/ {
+pub enum Op<'a> {
     NULL,
-    OP {opcode: opcode, sibling: Box<Op>, name: Option<String>, typ: Option<String>},
-    UNOP {opcode: opcode, sibling: Box<Op>, first: Box<Op>},
-    BINOP {opcode: opcode, sibling: Box<Op>, first: Box<Op>, last: Box<Op>},
-    LOGOP {opcode: opcode// , sibling: Box<Op>, first: Box<Op>, other: Box<Op>
+    OP {opcode: opcode, sibling: &'a Op<'a>, name: Option<String>, typ: Option<String>},
+    UNOP {opcode: opcode, sibling: &'a Op<'a>, first: &'a Op<'a>},
+    BINOP {opcode: opcode, sibling: &'a Op<'a>, first: &'a Op<'a>, last: &'a Op<'a>},
+    LOGOP {opcode: opcode// , sibling: &'a Op<'a>, first: &'a Op<'a>, other: &'a Op<'a>
     },
-    LISTOP {opcode: opcode// , sibling: Box<Op>, first: Box<Op>, last: Box<Op>
+    LISTOP {opcode: opcode// , sibling: &'a Op<'a>, first: &'a Op<'a>, last: &'a Op<'a>
     },
-    PMOP {opcode: opcode// , sibling: Box<Op>, first: Box<Op>, last: Box<Op>
+    PMOP {opcode: opcode// , sibling: &'a Op<'a>, first: &'a Op<'a>, last: &'a Op<'a>
     },
     SVOP (opcode, Sv),
     PADOP (opcode, Sv),
     PVOP (opcode/*, &'a pvop*/),
-    LOOP {opcode: opcode// , sibling: Box<Op>, first: Box<Op>, last: Box<Op>, redoop: Box<Op>, next: Box<Op>, last: Box<Op>
+    LOOP {opcode: opcode// , sibling: &'a Op<'a>, first: &'a Op<'a>, last: &'a Op<'a>, redoop: &'a Op<'a>, next: &'a Op<'a>, last: &'a Op<'a>
     },
     COP (opcode/*, &'a cop*/),
     METHOP(opcode, Name),
-    // {opcode: opcode, sibling: Box<Op>, name: Name},
+    // {opcode: opcode, sibling: &'a Op<'a>, name: Name},
     #[cfg(perlapi_ver26)]
     UNOP_AUX(opcode),
 }
 
 #[cfg(perlapi_ver26)]
-pub fn op_extract(perl: &Perl, cv: *const cv, o: *const op) -> Op {
-    if o.is_null() {
-        return Op::NULL
+impl<'a> OpExtractor<'a> {
+    
+    pub fn new(perl: &'a Perl) -> Self {
+        Self {perl, ops: Arena::new()}
     }
-    let cls = perl.op_class(o);
-    let oc = unsafe {
-        let ty = (*o).op_type();
-        *(&ty as *const u32 as *const opcode)
-    };
-    match cls {
-        OPclass::OPclass_NULL => Op::NULL,
-        OPclass::OPclass_BASEOP => {
-            let op = unsafe {o.as_ref().unwrap()};
-            let sibling = Box::new(op_extract(&perl, cv, op_sibling(o as *const unop)));
-            if let Some(pl) = cv_padnamelist(cv) {
-                if let Some(padname) = padnamelist_nth(pl, op.op_targ as usize) {
-                    return Op::OP {
-                        opcode: oc, name: PadnamePV(padname), typ: PadnameTYPE(padname),
-                        sibling
+
+    pub fn extract(&self, cv: *const cv, o: *const op) -> &'a Op {
+        if o.is_null() {
+            return self.ops.alloc(Op::NULL)
+        }
+        let cls = self.perl.op_class(o);
+        let oc = unsafe {
+            let ty = (*o).op_type();
+            *(&ty as *const u32 as *const opcode)
+        };
+
+        let eo = match cls {
+            OPclass::OPclass_NULL => Op::NULL,
+            OPclass::OPclass_BASEOP => {
+                let op = unsafe {o.as_ref().unwrap()};
+                let sibling = self.extract(cv, op_sibling(o as *const unop));
+                if let Some(pl) = cv_padnamelist(cv) {
+                    if let Some(padname) = padnamelist_nth(pl, op.op_targ as usize) {
+                        Op::OP {
+                            opcode: oc, name: PadnamePV(padname), typ: PadnameTYPE(padname),
+                            sibling
+                        }
+                    } else {
+                        Op::OP {opcode: oc, name: None, typ: None, sibling}
                     }
+                } else {
+                    Op::OP {opcode: oc, name: None, typ: None, sibling}
                 }
-            }
-            Op::OP {opcode: oc, name: None, typ: None, sibling}
-        },
-        OPclass::OPclass_UNOP => {
-            let op = unsafe {(o as *const unop).as_ref()}.unwrap();
-            let sibling = Box::new(op_extract(&perl, cv, op_sibling(o as *const unop)));
-            Op::UNOP {opcode: oc, sibling, first: Box::new(op_extract(&perl, cv, op.op_first))}
-        },
-        OPclass::OPclass_BINOP => {
-            let op = unsafe {(o as *const binop).as_ref()}.unwrap();
-            let sibling = Box::new(op_extract(&perl, cv, op_sibling(o as *const unop)));
-            Op::BINOP{
-                opcode: oc, sibling,
-                first: Box::new(op_extract(&perl, cv, op.op_first)),
-                last: Box::new(op_extract(&perl, cv, op.op_last)),
-            }
-        },
-        OPclass::OPclass_LOGOP => Op::LOGOP {opcode: oc},
-        OPclass::OPclass_LISTOP => Op::LISTOP {opcode: oc},
-        OPclass::OPclass_PMOP => Op::PMOP{opcode: oc},
-        OPclass::OPclass_SVOP => {
-            let sv = op_sv_or(o, |op| PAD_BASE_SV(CvPADLIST(cv), op.op_targ));
-            Op::SVOP(oc, sv_extract(sv))
-        },
-        OPclass::OPclass_PADOP => {
-            let op = unsafe {(o as *const padop).as_ref()}.unwrap();
-            let sv = PAD_BASE_SV(CvPADLIST(cv), op.op_padix);
-            Op::PADOP(oc, sv_extract(sv))
-        },
-        OPclass::OPclass_PVOP => Op::PVOP (oc),
-        OPclass::OPclass_LOOP => Op::LOOP {opcode: oc},
-        OPclass::OPclass_COP => Op::COP (oc),
-        OPclass::OPclass_METHOP => {
-            if (unsafe {*o}.op_flags & OPf_KIDS as u8) != 0 {
-                Op::METHOP (oc, Name::Dynamic)
-                
-            } else {
+            },
+            OPclass::OPclass_UNOP => {
+                let op = unsafe {(o as *const unop).as_ref()}.unwrap();
+                let sibling = self.extract(cv, op_sibling(o as *const unop));
+                Op::UNOP {opcode: oc, sibling, first: self.extract(cv, op.op_first)}
+            },
+            OPclass::OPclass_BINOP => {
+                let op = unsafe {(o as *const binop).as_ref()}.unwrap();
+                let sibling = self.extract(cv, op_sibling(o as *const unop));
+                Op::BINOP{
+                    opcode: oc, sibling,
+                    first: self.extract(cv, op.op_first),
+                    last: self.extract(cv, op.op_last),
+                }
+            },
+            OPclass::OPclass_LOGOP => Op::LOGOP {opcode: oc},
+            OPclass::OPclass_LISTOP => Op::LISTOP {opcode: oc},
+            OPclass::OPclass_PMOP => Op::PMOP {opcode: oc},
+            OPclass::OPclass_SVOP => {
                 let sv = op_sv_or(o, |op| PAD_BASE_SV(CvPADLIST(cv), op.op_targ));
-                Op::METHOP(oc, Name::Const(sv_extract(sv)))
-            }
-        },
-        #[cfg(perlapi_ver26)]
-        OPclass::OPclass_UNOP_AUX => Op::UNOP_AUX(oc /*, unsafe {(o as *const unop_aux).as_ref()}.unwrap()*/),
+                Op::SVOP(oc, sv_extract(sv))
+            },
+            OPclass::OPclass_PADOP => {
+                let op = unsafe {(o as *const padop).as_ref()}.unwrap();
+                let sv = PAD_BASE_SV(CvPADLIST(cv), op.op_padix);
+                Op::PADOP(oc, sv_extract(sv))
+            },
+            OPclass::OPclass_PVOP => Op::PVOP (oc),
+            OPclass::OPclass_LOOP => Op::LOOP {opcode: oc},
+            OPclass::OPclass_COP => Op::COP (oc),
+            OPclass::OPclass_METHOP => {
+                if (unsafe {*o}.op_flags & OPf_KIDS as u8) != 0 {
+                    Op::METHOP (oc, Name::Dynamic)
+                        
+                } else {
+                    let sv = op_sv_or(o, |op| PAD_BASE_SV(CvPADLIST(cv), op.op_targ));
+                    Op::METHOP(oc, Name::Const(sv_extract(sv)))
+                }
+            },
+            #[cfg(perlapi_ver26)]
+            OPclass::OPclass_UNOP_AUX =>
+                Op::UNOP_AUX(oc /*, unsafe {(o as *const unop_aux).as_ref()}.unwrap()*/),
+        };
+        
+        self.ops.alloc(eo)
     }
 }
