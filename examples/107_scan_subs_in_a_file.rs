fbfe6064e587be1768180f6b6b4516c99f54dbb8
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-12-28 12:03:21 +0900

    StashWalker built and worked!

--- examples/107_scan_subs_in_a_file.rs
+++ examples/107_scan_subs_in_a_file.rs
@@ -30,29 +30,35 @@ impl<'a> OpWalker<'a> {
 
 #[cfg(perlapi_ver26)]
 type Seen = std::collections::HashMap<String, bool>;
-type Filter = Option<dyn Fn(*const libperl_sys::cv) -> bool>;
-type Emitter = Option<dyn Fn(&String, *const libperl_sys::cv)>;
 
 #[cfg(perlapi_ver26)]
-pub struct StashWalker<'a> {
+pub struct StashWalker<'a, F, E>
+where F: FnMut(*const libperl_sys::cv) -> bool,
+      E: FnMut(&String, *const libperl_sys::cv)
+{
     pub perl: &'a Perl,
     pub seen: Seen,
+    filter: Option<&'a F>,
+    emitter: &'a mut E,
 }
 
 #[cfg(perlapi_ver26)]
-impl<'a> StashWalker {
+impl<'a, F, E> StashWalker<'a, F, E>
+where F: Fn(*const libperl_sys::cv) -> bool,
+      E: FnMut(&String, *const libperl_sys::cv) 
+ {
 
-    pub fn new(perl: &'a Perl) -> Self {
-        let seen = Seen::new();
+    pub fn new(perl: &'a Perl,
+               filter: Option<&'a F>,
+               emitter: &'a mut E) -> Self {
+        let mut seen = Seen::new();
         seen.insert("main".to_string(), true); // To avoid main::main::main...
         Self {
-            perl: &perl, seen, filter: None, emitter: None
+            perl: &perl, seen, filter, emitter
         }
     }
 
-    pub fn walk<F, E>(&self, filter: &'a F, emitter: &'a E)
-    where F: FnMut(*const libperl_sys::cv) -> bool,
-          E: FnMut(&String, *const libperl_sys::cv) {
+    pub fn walk(&mut self, pack: &str) {
     
         if self.seen.contains_key(pack) {return};
         self.seen.insert(pack.to_string(), true);
@@ -65,15 +71,15 @@ impl<'a> StashWalker {
 
             // ref $main::{foo} eq 'CODE'
             if let Some(Sv::CODE(cv)) = SvRV(item).map(|sv| sv_extract(sv)) {
-                if filter.map_or(true, |f| f(cv)) {
-                    emitter(&name, cv);
+                if (self.filter).map_or(true, |f| f(cv)) {
+                    (self.emitter)(&name, cv);
                 }
             }
             // ref (\$main::{foo}) eq 'GLOB'
             else if let Sv::GLOB {gv, ..} = sv_extract(item) {
                 let cv = GvCV(gv);
-                if filter.map_or(true, |f| f(cv)) {
-                    emitter(&name, cv);
+                if (self.filter).map_or(true, |f| f(cv)) {
+                    (self.emitter)(&name, cv);
                 }
                 if name.ends_with("::") {
                     // println!("package name = {}", name);
@@ -97,20 +103,21 @@ fn my_test() {
     let mut perl = Perl::new();
     perl.parse_env_args(env::args(), env::vars());
     
-    let mut nswalker = StashWalker::new(&perl);
-
     let main_file = sv_extract_pv(perl.get_sv("0", 0)).unwrap();
     println!("$0 = {:?}", main_file);
     
-    nswalker.walk(
-        |cv| CvFILE(cv).map_or(false, |s| &s == main_file),
-        |name: &String, cv: *const libperl_sys::cv| {
-            let walker = OpWalker {perl: &perl, cv};
-            println!("sub {:?}", name);
-            walker.walk(CvROOT(cv), 0);
-            println!("");
-        },
-    )
+    let filter = |cv| CvFILE(cv).map_or(false, |s| s == main_file);
+
+    let mut emitter = |name: &String, cv: *const libperl_sys::cv| {
+        let walker = OpWalker {perl: &perl, cv};
+        println!("sub {:?}", name);
+        walker.walk(CvROOT(cv), 0);
+        println!("");
+    };
+
+    let mut nswalker = StashWalker::new(&perl, Some(&filter), &mut emitter);
+
+    nswalker.walk("");
 }
 
 #[cfg(not(perlapi_ver26))]
