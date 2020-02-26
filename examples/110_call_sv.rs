9f945b3806fc858047643fb59e5e95b0146e9b92
Kobayasi, Hiroaki <buribullet@gmail.com>
2020-02-26 17:51:01 +0900

    無理やり Perl_call_pv を呼び出してみた.

--- examples/110_call_sv.rs
+++ examples/110_call_sv.rs
@@ -4,25 +4,40 @@ use libperl_sys::*;
 
 use std::ffi::CString;
 
+#[allow(unused_mut)]
+#[cfg(perl_useithreads)]
 fn my_test() {
     let mut perl = Perl::new();
     perl.parse_env_args(env::args(), env::vars());
     
+    let mut my_perl = unsafe {*(perl.my_perl)};
+
     // dSP
-    let mut sp = perl.my_perl.Istack_sp;
+    let mut sp = my_perl.Istack_sp;
+
     // PUSHMARK(SP)
-    perl.my_perl.Imarkstack_ptr = perl.my_perl.Imarkstack_ptr.add(1);
-    if perl.my_perl.Imarkstack_ptr == perl.my_perl.Imarkstack_max {
-        markstack_grow(perl.my_perl);
+    unsafe {
+        my_perl.Imarkstack_ptr = my_perl.Imarkstack_ptr.add(1)
+    };
+    if my_perl.Imarkstack_ptr == my_perl.Imarkstack_max {
+        unsafe {
+            Perl_markstack_grow(perl.my_perl)
+        };
     }
     unsafe {
-        *(perl.my_perl.Imarkstack_ptr)
-            = (sp as usize - perl.my_perl.Istack_base as usize) as i32;
+        *(my_perl.Imarkstack_ptr)
+            = (sp as usize - my_perl.Istack_base as usize) as i32;
     }
     
-    let subname = CString::new("foo");
+    let subname = CString::new("foo").unwrap();
 
-    call_sv(perl.my_perl, subname.as_ptr(), G_DISCARD);
+    unsafe {
+        Perl_call_pv(perl.my_perl, subname.as_ptr(), G_DISCARD as i32)
+    };
+}
+
+#[cfg(not(perl_useithreads))]
+fn my_test() {
 }
 
 fn main() {
