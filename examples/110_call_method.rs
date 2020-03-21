21935cc4d347938e24d8f3d23a1b3e958f6d67ca
Kobayasi, Hiroaki <buribullet@gmail.com>
2020-03-21 16:56:20 +0900

    Merge argument pushing

--- examples/110_call_method.rs
+++ examples/110_call_method.rs
@@ -73,15 +73,7 @@ fn call_list_method(perl: &mut Perl, class_name: String, method_name: String, ar
         sp = perl_api!{Perl_stack_grow(perl.my_perl, sp, sp, (1 + args.len()).try_into().unwrap())};
     }
     
-    {
-        let sv = perl.str2svpv_flags(class_name.as_str(), SVf_UTF8 | SVs_TEMP);
-        unsafe {
-            sp = sp.add(1);
-            *sp = sv;
-        }
-    }
-
-    for s in args {
+    for s in [&[class_name], args.as_slice()].concat() {
         let sv = perl.str2svpv_flags(s.as_str(), SVf_UTF8 | SVs_TEMP);
         unsafe {
             sp = sp.add(1);
