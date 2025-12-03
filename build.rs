3c56a342ab3a00414dfd171ff7430e8fd30f23db
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-12-03 18:11:03 +0900

    Add comment for cdylib cargo_ldopts

--- build.rs
+++ build.rs
@@ -2,6 +2,9 @@ use libperl_config::*;
 
 fn main() {
     let perl = PerlConfig::default();
+
+    // This is only needed when building a library crate (cdylib/staticlib).
+    // For binary crates, you can omit this line.
     perl.emit_cargo_ldopts();
 
     perl.emit_features(&["useithreads"]);
