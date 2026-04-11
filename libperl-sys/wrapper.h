9264535fad4c7ff065e2ffec352b9d056021d502
Kobayasi, Hiroaki <buribullet@gmail.com>
2026-04-11 12:35:20 +0900

    Use EXTERN.h instead of INTERN.h (for PL_Yes, PL_No...)

--- libperl-sys/wrapper.h
+++ libperl-sys/wrapper.h
@@ -1,4 +1,4 @@
-#include <INTERN.h>               /* from the Perl distribution     */
+#include <EXTERN.h>               /* from the Perl distribution     */
 #define PERL_IN_GLOBALS_C
 #include <perl.h>                 /* from the Perl distribution     */
 
