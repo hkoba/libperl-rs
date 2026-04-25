bb52d3f71817b1dcb0c2558f9166405ca60d71be
Kobayasi, Hiroaki <buribullet@gmail.com>
2026-04-25 14:40:02 +0900

    Suppress more warnings/errors

--- libperl-sys/src/perl_core.rs
+++ libperl-sys/src/perl_core.rs
@@ -3,6 +3,10 @@
 #![allow(non_snake_case)]
 #![allow(unnecessary_transmutes)]
 #![allow(unpredictable_function_pointer_comparisons)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+#![allow(unused_unsafe)]
+#![allow(unused_variables)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
